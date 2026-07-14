622fecab6bfbd36d1c8aa98cbd84ca0bc1c047c4
attkn <beyen22@itu.edu.tr>
2026-07-14 14:01:55 +0300

    risc-v pkg file updated

--- rtl/pkg/riscv_pkg.sv
+++ rtl/pkg/riscv_pkg.sv
@@ -11,6 +11,21 @@ package riscv_pkg;
     localparam int          ALU_OP_DIV = 4;
     localparam int          ALU_OP_BRANCH = 7;
     localparam int          MUL_LAT = 3;
+
+
+    localparam              ISA_I       =   40;
+    localparam              ISA_M       =   8;
+    localparam              ISA_CSR     =   6; 
+    localparam              ISA_FENCE   =   1;
+    localparam              ISA_A       =   11;
+    localparam              ISA_C       =   29;
+    localparam              ISA_B       =   29;
+    localparam              ISA_BC      =   3;
+    localparam              ISA_F       =   26;
+//    localparam              ISA_V       =  
+
+
+
     typedef enum logic [2:0] { 
         IMM_I, 
         IMM_S, 
@@ -56,4 +71,91 @@ package riscv_pkg;
         B
     }alu_op_branch_e;
     
+    typedef enum logic [$clog2(ISA_I)-1:0] {
+        LUI,
+        AUIPC,
+        JAL,
+        JALR,
+        BEQ,
+        BNE,
+        BLT,
+        BLTU,
+        LB,
+        LH,
+        LW,
+        LBU,
+        LHU,
+        SB,
+        SH,
+        SW,
+        ADDI,
+        SLTI,
+        SLTIU,
+        XORI,
+        ORI,
+        ANDI,
+        SLLI,
+        SRLI,
+        SRAI,
+        ADD,
+        SUB,
+        SLL,
+        SLT,
+        SLTU,
+        XOR,
+        SRL,
+        SRA,
+        OR,
+        AND,
+        FENCE,
+        ECALL,
+        EBREAK
+    }isa_i;
+
+
+    typedef enum logic [$clog2(ISA_CSR)-1:0] {
+        CSRRW,
+        CSRRS,
+        CSRRC,
+        CSRRWI,
+        CSRRSI,
+        CSRRCI,
+        FENCEI
+    }isa_csr;
+
+    typedef enum logic [$clog2(ISA_M)-1:0] {
+        MUL,MULH,MULHSU,MULHU,
+        DIV,DIVU,
+        REM,REMU
+    }isa_m;
+
+    typedef enum logic [$clog2(ISA_A)-1:0] {
+        LRW,SCW,
+        AMOSWAPW,AMOADDW,AMOXORW,AMOANDW,AMOORW,
+        AMOMINW,AMOMAXW,AMOMINU,AMOMAXUW
+    }isa_a;
+
+    typedef enum logic [$clog2(ISA_C)-1:0] {
+        CADDI4SPN,
+        CLW,CSW,CFLW,CFSW,
+        CNOP_CADDI,
+        CJAL,
+        CLI,
+        CLUI_CADDI16SP,
+        CSRLI,
+        CSRAI,
+        CANDI,
+        CSUB,
+        CXOR,
+        COR,
+        CAND,
+        CJ,
+        CBEQZ
+        CBNEZ,
+        CSLLI,
+        CLWSP,
+        CFLWSP,CJR,CMV,CEBREAK,
+        CJALR,CADD,
+        CSWSP,CFSWSP
+    }isa_c;
 endpackage
